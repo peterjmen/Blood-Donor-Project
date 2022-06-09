986212cec274b09ee479c0513e20a9d802be3b58
Peter menhinick <peterjmen@gmail.com>
2022-06-09 17:37:42 +1200

    Valid input checker improved
    Adjusted input checker to only allow from numbers 1-9, all other input will error and as for repeated input

--- Valid Input Function.cpp
+++ Valid Input Function.cpp
@@ -4,59 +4,36 @@
 using namespace std;
 
 
-void menu(string&input);
-void validInputCheckerFromOneToTen(string& input);
+void menu(string& input);
+void validInputCheckerFromOneToNine(string& input);
 
 
 int main() {
-	
+
 	string input;
 	menu(input);
-
 
 
 	system("pause > 0");
 }
 
-void menu(string &input) {
+void menu(string& input) {
 	cout << "Please choose and option :";
 	cout << "\n1. Yes";
 	cout << "\n2. No";
 	cout << "\n3. Exit\n";
+	
 	getline(cin, input);
-	validInputCheckerFromOneToTen(input);
-	cout << "\nContinue to to more things!\n";
-	//int stringToInt = input[0]-48;
-	///*(int)input[0]*/
-	//switch (stringToInt) {
-	//case 1:
-	//	cout << "You chose option " << stringToInt << ".\n";
-	//	break;
-	//case 2:
-	//	cout << "You chose option " << stringToInt << ".\n";
-	//	break;
-	//case 3:
-	//	cout << "You chose option " << stringToInt <<". Exit, goodbye\n\n\n";
-	//	return;
-	//	break;
-	//case 4:
-	//case 5:
-	//case 6:
-	//case 7:
-	//case 8:
-	//case 9:
-	//	cout << "This option has not been implemented yet.\n";
-	//	break;
-	//default:
-	//	cout << "\"" <<input << "\"" << "is not valid input, please try again\n";
-	//}
+	validInputCheckerFromOneToNine(input);
+	cout << "\nContinue to more things!\n";
 
-		menu(input);
+
+	menu(input);
 }
 
- void validInputCheckerFromOneToTen(string & input){
+void validInputCheckerFromOneToNine(string& input) {
 	int stringToInt = input[0] - 48;
-	if (stringToInt <1 || stringToInt > 10){
+	if (stringToInt < 1 || stringToInt > 9 || input.size() > 1) { //errors if anything except numbers from 1-9 are entered.
 	cout << "\"" << input << "\"" << "is not valid input, please try again entering a number from 1-10\n";
 	menu(input);
 	}
@@ -65,9 +42,3 @@ void menu(string &input) {
 	}
 
 }
-
-
-
-//
-////convert first letter of string to int
-//cout << (int)input[0];
